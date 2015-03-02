573bf19a90e2b0452cdeb64eafbfd5237c546a1c
James Read <jamesread01@yahoo.co.uk>
2015-03-02 21:48:56 +0000

    dynamic demo code

--- inc/demo-code.php
+++ inc/demo-code.php
@@ -1,35 +1,40 @@
 <?php
 
-$demo_code = '
-<ul class="da-thumbs">
-	<li class="grid">
-		<a href="#">
-			<img class="demo-image-size" src="http://lorempixel.com/400/200/sports/1" />
-			<div><span>Hello??</span></div>
-		</a>
-	</li>
-	<li class="grid">
-		<a href="#">
-			<img class="demo-image-size" src="http://lorempixel.com/400/200/sports/2" />
-			<div><span>HELLO!!</span></div>
-		</a>
-	</li>
-<!-- <div style="clear:both"></div> -->
+	$hover_board_parent_class = $options['hover_board_inner_class_name'];
+	$hover_board_child_class = $options['hover_board_outer_class_name'];
+	$type_of_container = ".";
+	$hover_board_combined_containers = $type_of_container . $hover_board_container_one . " > " . $hover_board_container_two;
+	// echo "this" . $hover_board_combined_containers;
 
-<li class="grid">
-		<a href="#">
-			<img class="demo-image-size" src="http://lorempixel.com/400/200/sports/3" />
-			<div><span>HEEELLOOOO</span></div>
-		</a>
-	</li>
-<li class="grid">
-		<a href="#">
-			<img class="demo-image-size" src="http://lorempixel.com/400/200/sports/4" />
-			<div><span>GOODBYEEE!!</span></div>
-		</a>
-	</li>
-	
-</ul>' ;
+$demo_code = '
+<div class="da-thumbs">
+	<ul class="'.$hover_board_parent_class.'">
+		<li class="'.$hover_board_child_class.'">
+			<a href="#">
+				<img class="demo-image-size" src="http://lorempixel.com/400/200/sports/1"/>
+				<div><span>Hello??</span></div>
+			</a>
+		</li>
+		<li class="'.$hover_board_child_class.'">
+			<a href="#">
+				<img class="demo-image-size" src="http://lorempixel.com/400/200/sports/2"/>
+				<div><span>HELLO!!</span></div>
+			</a>
+		</li>
+		<li class="'.$hover_board_child_class.'">
+				<a href="#">
+					<img class="demo-image-size" src="http://lorempixel.com/400/200/sports/3"/>
+					<div><span>HEEELLOOOO</span></div>
+				</a>
+			</li>
+		<li class="'.$hover_board_child_class.'">
+				<a href="#">
+					<img class="demo-image-size" src="http://lorempixel.com/400/200/sports/4"/>
+					<div><span>GOODBYEEE!!</span></div>
+				</a>
+			</li>
+	</ul>
+</div>' ;
 
 $code_output = htmlspecialchars($demo_code);
 ?>
